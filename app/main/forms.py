3b15e9452f17a7f83226c868435a0ef42bf4718f
wrenlearn <550.75693@qq.com>
2016-07-21 09:46:56 +0800

    10d

---
+++ app/main/forms.py
@@ -0,0 +1,45 @@
+# -*- coding=utf-8 -*-
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(Form):
+    name = StringField('真实姓名', validators=[Length(0, 64)])
+    location = StringField('所在地', validators=[Length(0, 64)])
+    about_me = TextAreaField('自我介绍')
+    submit = SubmitField('提交')
+
+class EditProfileAdminForm(Form):
+    email = StringField('邮箱', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('用户名', validators=[Required(), Length(1, 64),\
+            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, '用户名必须为字母、数字、点或下划线。')])
+    confirmed = BooleanField('是否认证')
+    role = SelectField('职务', coerce=int)
+    name = StringField('真实姓名', validators=[Length(0, 64)])
+    location = StringField('所在地', validators=[Length(0, 64)])
+    about_me = TextAreaField('自我介绍')
+    submit = SubmitField('提交')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) 
+                for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已注册！')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经被使用！')
